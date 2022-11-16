2341f8c082defa31cd171600077ff8492d2290ed
Ross Hunter <rhunter82@hotmail.com>
2022-11-16 21:25:09 +0100

    ex2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # resources :restaurants, only: [:index, :new, :create, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
 end
